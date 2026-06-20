b8c106cd4e6bcb5fa8dcd6ad2f3616a498bfb817
Nasreddine Boutouil <nasreddine021@gmail.com>
2026-06-20 15:26:08 +0200

    feat(frontend): add automatic token refresh on 401

--- packages/frontend/src/core/api/index.ts
+++ packages/frontend/src/core/api/index.ts
@@ -3,10 +3,32 @@ import { hc } from "hono/client";
 
 import { HttpError } from "@/shared/errors/http-error";
 
+import { router } from "../router";
+
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const customFetch: typeof fetch = async (input, init) => {
   let res = await fetch(input, init);
+
+  const url = input.toString();
+
+  const isAuthEndpoint =
+    url.endsWith("/register") ||
+    url.endsWith("/login") ||
+    url.endsWith("/refresh");
+
+  if (res.status == 401 && !isAuthEndpoint) {
+    const refreshRes = await fetch(`${BASE_URL}/refresh`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (!refreshRes.ok) {
+      router.push({ name: "login" });
+    } else {
+      res = await fetch(input, init);
+    }
+  }
 
   if (!res.ok) {
     const data = await res.json().catch(() => undefined);
